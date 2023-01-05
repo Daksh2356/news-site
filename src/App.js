a7182c6ccef78f37416b3f2058f9ee01b8a54640
Daksh2356 <dakshmakhija@gmail.com>
2023-01-06 01:27:11 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FAQ from "./pages/FAQ";
+import Home from "./pages/Home";
 
 function App() {
+  let x = false;
+
+  // bool x is used to separate two pages 
 
   return (
     <div class="flex justify-center">
+      {x ? <Home /> : <FAQ />}
 
-  <div>
-    <strong className='text-orange-800'>Andrew Alfred</strong>
-    <span>Technical advisor</span>
-  </div>
-</div>
+      <div>
+        <strong className="text-orange-800">Andrew Alfred</strong>
+        <span>Technical advisor</span>
+      </div>
+    </div>
   );
 }
 
