e21611503f5f0b5719746b2c677f057501947764
Nikhil <nikhilarora10b@gmail.com>
2023-03-16 21:30:40 +0530

    added page 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import FAQ from "./pages/FAQ.jsx";
 import Home from "./pages/Home";
+import Page1 from "./pages/page1";
 
 function App() {
   let x = true;
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/faq" element={<FAQ />} />
+      <Route path="/page1" element={<Page1 />} />
     </Routes>
   );
 }
